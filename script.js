f6c1f08eea236e700633e3fb4a25c126c8f9e2ee
Darko Peshevski <darko.peshevski2@gmail.com>
2023-08-23 16:42:13 +0200

    replaced the console.logs with alert() function.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let myScore = 0;
 let aiScore = 0;
 let gameLimit = 5;
 
+
 // a function to return a random result from the ai player .
 function getComputerChoice() {
   const aiPlayerArray = ['Rock', 'Paper', 'Scissors'];
@@ -19,9 +20,10 @@ function playRound(playerOne, aiPLayer) {
 
   let result;
 
-  if (playerOne == 'rock' && aiPLayer == 'rock') {
+  if (playerOne === aiPLayer) {
     result = "It's a tie!";
   } 
+
   else if (playerOne == 'rock' && aiPLayer == 'scissors') {
     result = "You win! rock beats scissors!";
   }
@@ -31,9 +33,7 @@ function playRound(playerOne, aiPLayer) {
   else if (playerOne == 'paper' && aiPLayer == 'rock') {
     result = 'You win! paper beats rock!';
   }
-  else if (playerOne == 'paper' && aiPLayer == 'paper') {
-    result = "It's a tie!";
-  }
+  
   else if (playerOne == 'paper' && aiPLayer == 'scissors') {
     result = 'The AI player wins. scissors beats paper.';
   }
@@ -43,9 +43,7 @@ function playRound(playerOne, aiPLayer) {
   else if (playerOne == 'scissors' && aiPLayer == 'paper') {
     result = 'You win! scissors beats paper.';
   }
-  else if (playerOne == 'scissors' && aiPLayer == 'scissors') {
-    result = 'It is a tie!';
-  }
+
   else {
     result = 'Wrong input, try again';
   }
@@ -57,6 +55,7 @@ function playRound(playerOne, aiPLayer) {
   return result;
 }
 
+
 //main function game
 function game() {
   
@@ -64,7 +63,7 @@ function game() {
   const myAnswerFormatted = myAnswer.toLowerCase();
 
   let aRound = playRound(myAnswerFormatted, getComputerChoice());
-  console.log(aRound);
+  alert(aRound);
 
   // making some variables to hold occurence of words so I can compare them later
   let occurenceOne = 'You';
@@ -81,28 +80,28 @@ function game() {
     aiScore++ ;
   }
   
-  console.log(`Your score: ${myScore}\nComputer score: ${aiScore}`);
+  alert(`Your score: ${myScore}\nComputer score: ${aiScore}`);
+  
 
   // checking to see if i need to decrement or increment the global 'gameLimit' variable.
   if (aRound == 'Wrong input, try again') {
-    gameLimit-- ;
-    gameLimit++ ;
+    // If it's a wrong input, the gameLimit remains unchanged.
   } else {
     gameLimit-- ;
   }
-  
 
-  console.log(`${gameLimit} rounds left!`);
+  alert(`${gameLimit} rounds left!`);
+
 
   // if/else conditionals check for end game.
   if (myScore > aiScore && gameLimit == 0) {
-    console.log('YOU WON!');
+    alert('YOU WON!');
   }
   else if (myScore < aiScore && gameLimit == 0) {
-    console.log('THE PC WON!');
+    alert('THE PC WON!');
   }
   else if (myScore == aiScore && gameLimit == 0) {
-    console.log('It is a tie!');
+    alert('It is a tie!');
   }
 
 }
